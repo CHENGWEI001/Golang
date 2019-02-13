ea05a8aa4f7bfeef85dfed07de941cf0a60a308a
EC2 Default User <ec2-user@ip-172-31-37-125.us-west-2.compute.internal>
2019-02-13 08:57:47 +0000

    shape

---
+++ shape/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+func main() {
+	s := square{8.0}
+	t := triangle{1.5, 2.0}
+	printArea(s)
+	printArea(t)
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.height / 2
+}
